a66b2d8639d42841c06ef2b1d54330eccde57d7b
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-03-21 07:31:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstName" },
+    { message: "Enter second number", type: "number", name: "secondName" },
+    {
+        message: "Enter your operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstName + answer.secondName);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstName - answer.secondName);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstName * answer.secondName);
+}
+else if (answer.operator === "Divisions") {
+    console.log(answer.firstName / answer.secondName);
+}
+else {
+    console.log("Invalid operator");
+}
